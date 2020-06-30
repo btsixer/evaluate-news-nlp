d1b44e396983170b60473d9312eabe9485656c84
btsixer <60943928+btsixer@users.noreply.github.com>
2020-06-30 07:51:05 -0500

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,6 +5,19 @@ function handleSubmit(event) {
     let formText = document.getElementById('name').value
     checkForName(formText)
 
+    let url = document.getElementById('name').value;
+        fetch('/sentiment', {
+                method: "POST",
+                credentials: "same-origin",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ url:url })
+            })
+            .then(res => res.json())
+            .then(function(res) {
+                document.getElementById('name').innerHTML= res.(polarity)
+            })
+    }
+
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8080/test')
     .then(res => res.json())
