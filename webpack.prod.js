185a3353870b9d37e8c3d85d668a254887549366
btsixer <60943928+btsixer@users.noreply.github.com>
2020-07-15 08:41:20 -0500

    Add files via upload

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const path = require('path')
 const webpack = require('webpack')
@@ -31,7 +32,8 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-	new MiniCssExtractPlugin({filename: '[name].css'})
+        new MiniCssExtractPlugin({filename: '[name].css'}),
+        new WorkboxPlugin.GenerateSW()
     ],
     optimization: {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
